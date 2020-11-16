753669981ef9a8f98145ccfac86004fac81084d2
Apansenko Roman <romis_97@mail.ru>
2020-11-16 13:07:03 +0600

    create admin

--- articlesapp/admin.py
+++ articlesapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Article
+from .models import Author
+from .models import Category
 
-# Register your models here.
+
+admin.site.register(Article)
+admin.site.register(Author)
+admin.site.register(Category)
