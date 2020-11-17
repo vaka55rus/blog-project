1e38c24a3bc1bcb93f5b9edbb683cb1966b4aadb
Apansenko Roman <romis_97@mail.ru>
2020-11-17 16:32:28 +0600

    Fixed articled models

--- articlesapp/admin.py
+++ articlesapp/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from .models import Article
-from .models import Author
 from .models import Category
 
 
 admin.site.register(Article)
-admin.site.register(Author)
 admin.site.register(Category)
--- articlesapp/models.py
+++ articlesapp/models.py
@@ -1,32 +1,22 @@
 from django.db import models
 
-class Author(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.first_name
-
-class Category(models.Model):
-    category = models.CharField(max_length=300)
-
-    def __str__(self):
-        return self.category
-
 class Article(models.Model):
     title = models.CharField(max_length=300)
     body = models.TextField()
-    author = models.OneToOneField(
-        Author,
-        on_delete=models.CASCADE,
-    )
-    category = models.ForeignKey(
-        Category,
-        on_delete=models.CASCADE
-    )
-    date_create = models.DateTimeField()
-    date_modification = models.DateTimeField()
-    published = models.BooleanField(null=True)
+    date_create = models.DateTimeField(auto_now_add=True)
+    date_modification = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
+
+class Category(models.Model):
+    category = models.CharField(max_length=300)
+    articleid = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE,
+        null = True,
+    )
+
+    def __str__(self):
+        return self.category
