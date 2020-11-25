a76594394d5193522c9bd521794dd01e96cfdc02
Apansenko Roman <romis_97@mail.ru>
2020-11-25 16:26:53 +0600

    rename article_detail

--- articlesapp/urls.py
+++ articlesapp/urls.py
@@ -4,7 +4,7 @@ from articlesapp.views import ArticleView, ArticleDetailView, ArticleCreate,Arti
 
 urlpatterns = [
     path('', ArticleView.as_view(), name = 'article_list'),
-    path('<int:pk>/', ArticleDetailView.as_view(), name='post_detail'),
+    path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
     path('<pk>/update', ArticleUpdate.as_view(), name='update'),
     path('create', ArticleCreate.as_view(), name='create')
 ]
